813e517378201d663124d4d2a5c990c9f1c75d5c
Matias <mggutierrez@uc.cl>
2021-01-02 21:22:18 -0300

    Building our React App

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 const config = {
+  STRIPE_KEY:
+    "pk_test_51I5C5uGHpnWf8FrDt5WanX5NbGKUpEzojj5VN0lVEqnmHIndmplGFCrxBB9jAV3RgNx6OEp5tX38r6TfSZI3oSc400QrVEsmkj",
+  MAX_ATTACHMENT_SIZE: 5 * 1000 * 1000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-menita",
